529eecd0f273122a74072d823eac8a3865aeb2fb
vijaybaliah <vijaybalu1989@gmail.com>
2019-10-06 21:04:06 +0530

    feat: added server side routing

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { hydrate } from 'react-dom'
 import App from '../shared/App'
+import { BrowserRouter } from 'react-router-dom'
 
-hydrate(<App data={window.__INITIAL_DATA__}/>, document.getElementById('app'))
+hydrate(
+<BrowserRouter>
+  <App />
+</BrowserRouter>,
+document.getElementById('app'))
--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,8 @@ import App from '../shared/App'
 import React from 'react'
 import serialize from "serialize-javascript"
 import { fetchPopularRepos } from '../shared/api'
+import { StaticRouter, matchPath } from "react-router-dom"
+import routes from '../shared/routes'
 
 const app = express()
 
@@ -15,28 +17,38 @@ app.use(cors())
 // client bundle.js file will end up.
 app.use(express.static("public"))
 app.get("*", (req, res, next) => {
-  fetchPopularRepos()
-    .then(data => {
+  const activeRoute = routes.find(
+    (route) => matchPath(req.url, route)
+  ) || {}
+
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData(req.path)
+    : Promise.resolve()
+
+    promise.then((data) => {
+      const context = { data }
       const markup = renderToString(
-        <App data={data} />
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+        
       )
+  
       res.send(`
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <title>SSR with RR</title>
-              <script src='/bundle.js' defer></script>
-              <script>window.__INITIAL_DATA__=${serialize(data)}</script>
-            </head>
-            <body>
-              <div id="app">
-                ${markup}
-              </div>
-            </body>
-          </html>
-        `
-      )
-    })
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>SSR with RR</title>
+            <script src="/bundle.js" defer></script>
+            <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+          </head>
+  
+          <body>
+            <div id="app">${markup}</div>
+          </body>
+        </html>
+      `)
+    }).catch(next)
 
 })
 
